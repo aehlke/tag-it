477488e48882a1f88d621164c2f437c7e7ca8e9d
Chris Leishman <chris@leishman.org>
2011-09-27 16:56:31 +0200

    Configuration of keyCodes that act as delimiters
    In addition to SPACE (if allowSpaces == false), ENTER and TAB.
    Defaults to $.ui.keyCode.COMMA.

--- js/tag-it.js
+++ js/tag-it.js
@@ -39,6 +39,9 @@
             // for inputting multi-word tags.
             allowSpaces: false,
 
+            // Tag delimiters to use in addition to space, enter and tab
+            delimiterKeyCodes: [$.ui.keyCode.COMMA],
+
             // Whether to animate tag removals or not.
             animate: true,
 
@@ -157,6 +160,10 @@
                 }
             }
 
+            if (this.options.allowSpaces !== true) {
+                this.options.delimiterKeyCodes.push($.ui.keyCode.SPACE);
+            }
+
             // Events.
             this._tagInput
                 .keydown(function(event) {
@@ -173,19 +180,19 @@
                         that._lastTag().removeClass('remove ui-state-highlight');
                     }
 
-                    // Comma/Space/Enter are all valid delimiters for new tags,
-                    // except when there is an open quote or if setting allowSpaces = true.
-                    // Tab will also create a tag, unless the tag input is empty, in which case it isn't caught.
+                    // Any keyCode in options.delimiterKeyCodes, in addition to
+                    // Enter, are valid delimiters for new tags except when
+                    // there is an open quote.
+                    // Tab will also create a tag, unless the tag input is
+                    // empty, in which case it isn't caught.
                     if (
-                        event.which == $.ui.keyCode.COMMA ||
                         event.which == $.ui.keyCode.ENTER ||
                         (
                             event.which == $.ui.keyCode.TAB &&
                             that._tagInput.val() !== ''
                         ) ||
                         (
-                            event.which == $.ui.keyCode.SPACE &&
-                            that.options.allowSpaces !== true &&
+                            ($.inArray(event.which, that.options.delimiterKeyCodes) >= 0) &&
                             (
                                 $.trim(that._tagInput.val()).replace( /^s*/, '' ).charAt(0) != '"' ||
                                 (
