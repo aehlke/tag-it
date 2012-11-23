9996532f9c142ae1b57f82118eb86b0f897d42d3
Alex Ehlke <alex.ehlke@gmail.com>
2012-11-22 19:56:46 -0500

    add gh-pages merge to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-# Minify our JS.
+echo "Compiling JS..."
 JS=./js/tag-it.js
 MINIFIED_OUTPUT=./js/tag-it.min.js
 curl -f -d compilation_level=SIMPLE_OPTIMIZATIONS -d output_format=text -d output_info=compiled_code --data-urlencode "js_code@${JS}" https://closure-compiler.appspot.com/compile -o $MINIFIED_OUTPUT
+
+echo "Updating GitHub pages..."
+if git checkout gh-pages; then
+    git merge master
+    git checkout master
+fi
 
