da305e7c87cacea30b9afb15be5872c1fccfa706
Alex Ehlke <alex.ehlke@gmail.com>
2012-11-22 19:43:28 -0500

    add build script for minifying js via closure compiler webservice

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Minify our JS.
+JS=./js/tag-it.js
+MINIFIED_OUTPUT=./js/tag-it.min.js
+curl -d compilation_level=SIMPLE_OPTIMIZATIONS -d output_format=text -d output_info=compiled_code --data-urlencode "js_code@${JS}" https://closure-compiler.appspot.com/compile > $MINIFIED_OUTPUT
+
