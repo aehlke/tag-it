38a28ee3b91a93edd399247e51a9f391da242aa5
Alex Ehlke <alex.ehlke@gmail.com>
2012-11-22 20:01:28 -0500

    clean up build script output

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 echo "Compiling JS..."
 JS=./js/tag-it.js
 MINIFIED_OUTPUT=./js/tag-it.min.js
-curl -f -d compilation_level=SIMPLE_OPTIMIZATIONS -d output_format=text -d output_info=compiled_code --data-urlencode "js_code@${JS}" https://closure-compiler.appspot.com/compile -o $MINIFIED_OUTPUT
+curl --progress-bar -f -d compilation_level=SIMPLE_OPTIMIZATIONS -d output_format=text -d output_info=compiled_code --data-urlencode "js_code@${JS}" https://closure-compiler.appspot.com/compile -o $MINIFIED_OUTPUT
 
+echo
 echo "Updating GitHub pages..."
-if git checkout gh-pages; then
+if git checkout gh-pages -q; then
     git merge master
-    git checkout master
+    git checkout master -q
 fi
 
