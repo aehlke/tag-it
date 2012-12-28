7f173f7f25b49ac3b2969c8c0c92894c8ca8a6b3
Jordan Hoff <jhoff@linkedin.com>
2012-12-28 16:55:24 -0600

    Undo whitespace cleanup ( sorry )

--- js/tag-it.js
+++ js/tag-it.js
@@ -60,7 +60,7 @@
             //
             // The easiest way to use singleField is to just instantiate tag-it
             // on an INPUT element, in which case singleField is automatically
-            // set to true, and singleFieldNode is set to that element. This
+            // set to true, and singleFieldNode is set to that element. This 
             // way, you don't need to fiddle with these options.
             singleField: false,
 
@@ -235,7 +235,7 @@
 
                     // Comma/Space/Enter are all valid delimiters for new tags,
                     // except when there is an open quote or if setting allowSpaces = true.
-                    // Tab will also create a tag, unless the tag input is empty,
+                    // Tab will also create a tag, unless the tag input is empty, 
                     // in which case it isn't caught.
                     if (
                         event.which === $.ui.keyCode.COMMA ||
