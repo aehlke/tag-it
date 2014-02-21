f59ea34c49803e47730ed414457e411bee7ea254
Carlos Alejandro Frias <carlosfrias64@gmail.com>
2014-02-21 00:36:17 -0600

    Added feature 'separateValueLabel'
    Added the feature to enable that when set separateValueLabel = true, the
    method createTag expects value to be an object returned from
    autocomplete, having the .value and .label properties, .value goes to
    hidden input, and .label stays in the tag.

--- js/tag-it.js
+++ js/tag-it.js
@@ -28,6 +28,7 @@
 
     $.widget('ui.tagit', {
         options: {
+			separateValueLabel: false,
             allowDuplicates   : false,
             caseSensitive     : true,
             fieldName         : 'tags',
@@ -283,7 +284,12 @@
             if (this.options.availableTags || this.options.tagSource || this.options.autocomplete.source) {
                 var autocompleteOptions = {
                     select: function(event, ui) {
-                        that.createTag(ui.item.value);
+						if(that.options.separateValueLabel){
+							that.createTag(ui.item);
+						}
+						else{
+							that.createTag(ui.item.value);
+						}
                         // Preventing the tag input to be updated with the chosen value.
                         return false;
                     }
@@ -439,7 +445,13 @@
         createTag: function(value, additionalClass, duringInitialization) {
             var that = this;
 
-            value = $.trim(value);
+			if(this.options.separateValueLabel) {				
+				var labelText = value.label;
+				value = $.trim(value.value);
+			}
+			else{
+				value = $.trim(value);
+			}
 
             if(this.options.preprocessTag) {
                 value = this.options.preprocessTag(value);
@@ -467,7 +479,8 @@
                 return false;
             }
 
-            var label = $(this.options.onTagClicked ? '<a class="tagit-label"></a>' : '<span class="tagit-label"></span>').text(value);
+			var txt = (this.options.separateValueLabel) ? labelText : value;
+            var label = $((this.options.onTagClicked ? '<a class="tagit-label"></a>' : '<span class="tagit-label"></span>')).text(txt);
 
             // Create tag.
             var tag = $('<li></li>')
@@ -494,8 +507,7 @@
 
             // Unless options.singleField is set, each tag has a hidden input field inline.
             if (!this.options.singleField) {
-                var escapedValue = label.html();
-                tag.append('<input type="hidden" value="' + escapedValue + '" name="' + this.options.fieldName + '" class="tagit-hidden-field" />');
+                tag.append('<input type="hidden" value="' + value + '" name="' + this.options.fieldName + '" class="tagit-hidden-field" />');
             }
 
             if (this._trigger('beforeTagAdded', null, {
